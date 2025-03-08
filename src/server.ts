a24ba8bb6d93c232901a8a828068cd770cf60120
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2025-03-08 04:49:39 +0300

    feat: add route paths

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,34 @@
 import express from "express";
 import { json } from "express";
-import sequelize from './config/database';
+import sequelize from "./config/database";
+import authMiddleware from "./middleware/auth";
+import userRoute from "./routes/User";
 import categoryRoute from "./routes/Category";
+import incomeRoute from "./routes/Income";
+import spendingRoute from "./routes/Spending";
 
 const app = express();
 app.use(json());
+app.use(userRoute);
 app.use(categoryRoute);
+app.use(incomeRoute);
+app.use(spendingRoute);
+
+app.get("/protected", authMiddleware, (req, res) => {
+  res.send("This is a protected route.");
+});
 
 const syncDatabase = async () => {
-    try {
-        await sequelize.sync({ force: false });
-        console.log('Database tables are synchronized!');
-    } catch (error) {
-        console.error('Sync error:', error);
-    }
+  try {
+    await sequelize.sync({ force: false });
+    console.log("Database tables are synchronized!");
+  } catch (error) {
+    console.error("Sync error:", error);
+  }
 };
 
 syncDatabase().then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is listening on ${process.env.PORT}`);
-    });
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on ${process.env.PORT}`);
+  });
 });
