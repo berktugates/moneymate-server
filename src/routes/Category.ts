783b0855f0b9d3232157a67a109cd1101e5a2918
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2025-03-08 04:47:56 +0300

    fix: update category route

--- src/routes/Category.ts
+++ src/routes/Category.ts
@@ -1,91 +1,17 @@
-import { Response, Router, Request } from "express";
-import { sendErrorResponse } from "../utils/sendErrorResponse";
-import Category, { validateCategory } from "../models/Category";
+import { Router } from "express";
+import CategoryController from "../controller/Category";
+import authMiddleware from "../middleware/auth";
 
 const router = Router();
 
-router.delete("/api/categories/:id", async (req: Request, res: Response) => {
-  try {
-    const category = await Category.findByPk(req.params.id);
-    if (!category) {
-      return sendErrorResponse(res, 404, "Category not found.");
-    }
-    const deletedCategory = await Category.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    return res
-      .status(200)
-      .send({
-        message: "Category deleted successfully.",
-        deleted: category,
-        amount: deletedCategory,
-      });
-  } catch (err) {
-    console.log(err);
-    return sendErrorResponse(res, 500, "Internal server error.");
-  }
-});
+router.delete("/api/categories/:id", authMiddleware, CategoryController.delete);
 
-router.put("/api/categories/:id", async (req: Request, res: Response) => {
-  try {
-    let category = await Category.findByPk(req.params.id);
-    if (!category) {
-      return sendErrorResponse(res, 404, "Category not found.");
-    }
-    const { error } = validateCategory(req.body);
-    if (error) {
-      return sendErrorResponse(res, 422, error.details[0].message);
-    }
-    await category.update({
-      name: req.body.name,
-    });
-    return res.status(200).send(category);
-  } catch (err) {
-    console.log(err);
-    return sendErrorResponse(res, 500, "Internal server error.");
-  }
-});
+router.put("/api/categories/:id", authMiddleware, CategoryController.update);
 
-router.post("/api/categories", async (req: Request, res: Response) => {
-  try {
-    const { error } = validateCategory(req.body);
-    if (error) {
-      return sendErrorResponse(res, 422, error.details[0].message);
-    }
-    const category = await Category.create({ name: req.body.name });
-    return res.status(201).send(category);
-  } catch (err) {
-    console.log(err);
-    return sendErrorResponse(res, 500, "Internal server error.");
-  }
-});
+router.post("/api/categories", authMiddleware, CategoryController.create);
 
-router.get("/api/categories/:id", async (req: Request, res: Response) => {
-  try {
-    const category = await Category.findByPk(req.params.id);
-    if (!category) {
-      return sendErrorResponse(res, 404, "Category not found.");
-    }
-    return res.status(200).send(category);
-  } catch (err) {
-    console.log(err);
-    sendErrorResponse(res, 500, "Internal server error");
-  }
-});
+router.get("/api/categories/:id", authMiddleware, CategoryController.get);
 
-router.get("/api/categories", async (req: Request, res: Response) => {
-  try {
-    const categories = await Category.findAll();
-    if (!categories) {
-      return sendErrorResponse(res, 404, "Categories not found.");
-    }
-    return res.status(200).send(categories);
-  } catch (err) {
-    console.log(err);
-    sendErrorResponse(res, 500, "Internal server error.");
-  }
-});
+router.get("/api/categories", authMiddleware, CategoryController.getAll);
 
 export default router;
